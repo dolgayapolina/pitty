0b2b223c8c7c0a4d70b011d564b23126f02d34a0
polina <palinagolgaya@yandex.ru>
2024-02-29 16:57:52 +0300

    привет марс

--- main.py
+++ main.py
@@ -25,5 +25,33 @@ def image_mars():
     return '''<h1> Жди нас, Марс! </h1><img src="/static/image/mars.png" alt="здесь должна была быть картинка, но не нашлась">'''
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return '''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" href="styles.css">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="/static/image/mars.png" alt="здесь должна была быть картинка, но не нашлась">
+                        <div class="alert alert-primary" role="alert">
+                          <br>Человечество вырастает из детства.</br>
+                          <br>Человечеству мала одна планета.</br>
+                          <br>Мы сделаем обитаемыми безжизненные пока планеты. </br>
+                          <br>И начнем с Марса!</br>
+                          <br>Присоединяйся!</br>
+                        </div>
+                      </body>
+                    </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
